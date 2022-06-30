e45d8748912e1d89f24230d720fc1e24c118af3b
kradle <atb279@gmail.com>
2022-06-30 15:51:36 -0500

    FizzBuzz Complete

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,24 @@
-print ("Hello world!")
-print ("1")
-msg = "This is a test to ensure my github repo is working."
-print (msg)
+
+var = []
+
+for i in range (1,100):		        #for loop for incrementing number to 100
+    FIZZ = False
+    BUZZ = False
+
+    if i%3 == 0:                    #if i is divisible by 3, FIZZ
+        FIZZ = True         
+    if i%5 == 0:                    #if i is divisible by 5, BUZZ
+        BUZZ = True
+
+    if not FIZZ:
+        if not BUZZ:
+            var.append(i)           #print original number if not divisible by 3 or 5
+        else:
+            var.append("BUZZ")
+    else:
+        if not BUZZ:
+            var.append("FIZZ")
+        else:
+            var.append("FIZZBUZZ")  #print FIZZBUZZ if divisible by both 3 and 5
+        
+print(var)
